dcd8a6c28758034a1040c75bd57f86c1a93577a4
Maulana-Aden <140379200+Maulana-Aden@users.noreply.github.com>
2026-01-06 20:51:18 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Navbar from "../components/Navbar";
+import Home from "../components/Home";
+import About from "../components/About";
+import Area from "../components/Area";
+import Break from "../components/Break";
+import Offers from "../components/Offers";
+import Menu from "../components/Menu";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Area />
+      <Break />
+      <Offers />
+      <Menu />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
